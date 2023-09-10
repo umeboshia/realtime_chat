d9350e4aa3c8dfddce71a311405eb737c5b62eb1
umeboshia <umebyoshia@gmail.com>
2023-09-11 01:57:00 +0900

    chap.8

--- chat-app/src/pages/_app.tsx
+++ chat-app/src/pages/_app.tsx
@@ -1,16 +1,20 @@
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import { initializeFirebaseApp } from '@src/lib/firebase/firebase'
-import { getApp } from 'firebase/app'
+// import { getApp } from 'firebase/app'
+import { AuthProvider } from '@src/feature/auth/provider/AuthProvider'
+import { Header } from '@src/component/Header/Header'
 
 initializeFirebaseApp()
 function MyApp({ Component, pageProps }: AppProps) {
-  console.log(getApp())
-  return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
-  );
+ return (
+   <ChakraProvider>
+      <AuthProvider>
+        <Header />
+       <Component {...pageProps} />
+      </AuthProvider>
+   </ChakraProvider>
+ )
 }
 
-export default MyApp;
+export default MyApp
