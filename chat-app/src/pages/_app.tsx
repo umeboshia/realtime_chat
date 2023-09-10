1f1ca1a456995b06787ac8e3702964af43babd19
umeboshia <umebyoshia@gmail.com>
2023-09-10 19:37:47 +0900

    chapter6

---
+++ chat-app/src/pages/_app.tsx
@@ -0,0 +1,16 @@
+import type { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
+import { initializeFirebaseApp } from '@src/lib/firebase/firebase'
+import { getApp } from 'firebase/app'
+
+initializeFirebaseApp()
+function MyApp({ Component, pageProps }: AppProps) {
+  console.log(getApp())
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
+}
+
+export default MyApp;
---
+++ chat-app/src/pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+import { Box, Heading } from '@chakra-ui/react'
+
+const Page: NextPage = () => {
+  return (
+    <Box>
+      <Heading>Chakra UI</Heading>
+    </Box>
+  )
+};
+
+export default Page;
