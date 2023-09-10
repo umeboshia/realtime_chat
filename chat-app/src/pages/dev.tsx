f68a7fc3cb4797424b9298e32225564b3f820b08
umeboshia <umebyoshia@gmail.com>
2023-09-11 00:45:48 +0900

    dev

---
+++ chat-app/src/pages/dev.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from "next";
+import { Box, Text } from '@chakra-ui/react'
+
+const Page: NextPage = () => {
+    return (
+      <Box>
+        <Text>dev</Text>
+      </Box>
+    )
+  };
+  
+  export default Page;
+  
