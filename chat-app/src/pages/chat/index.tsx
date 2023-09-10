95e852f6f4e0c29fae3893e6a8b95ac5aad5e298
umeboshia <umebyoshia@gmail.com>
2023-09-11 02:33:20 +0900

    chap12

--- chat-app/src/pages/chat/index.tsx
+++ chat-app/src/pages/chat/index.tsx
@@ -1,45 +1,93 @@
 import {
-    Button,
-    chakra,
-    Container,
-    Heading,
-    Input,
-    Spacer,
-   } from '@chakra-ui/react'
-   import { FormEvent, useState } from 'react'
-   import { getDatabase, push, ref } from '@firebase/database'
-   import { FirebaseError } from '@firebase/util'
-   
-   export const Page = () => {
-    const [message, setMessage] = useState<string>('')
-   
-     const handleSendMessage = async (e: FormEvent<HTMLFormElement>) => {
-      console.log(message)
-      e.preventDefault()
-       try {
-         const db = getDatabase()
-         const dbRef = ref(db, 'chat')
-         await push(dbRef, {
-           message,
-         })
-         setMessage('')
-       } catch (e) {
-         if (e instanceof FirebaseError) {
-           console.log(e)
-         }
-       }
-    }
-   
+    Avatar,
+    Box,
+   Button,
+   chakra,
+   Container,
+    Flex,
+   Heading,
+   Input,
+   Spacer,
+    Text,
+  } from '@chakra-ui/react'
+  import { FormEvent, useEffect, useState } from 'react'
+  import { getDatabase, onChildAdded, push, ref } from '@firebase/database'
+  import { FirebaseError } from '@firebase/util'
+  
+  const _message = '確認用メッセージです。'
+  const _messages = [...Array(10)].map((_, i) => _message.repeat(i + 1))
+  
+  type MessageProps = {
+    message: string
+  }
+  
+  const Message = ({ message }: MessageProps) => {
     return (
-      <Container py={14}>
-        <Heading>チャット</Heading>
-        <Spacer height={8} aria-hidden />
-        <chakra.form display={'flex'} gap={2} onSubmit={handleSendMessage}>
-          <Input value={message} onChange={(e) => setMessage(e.target.value)} />
-          <Button type={'submit'}>送信</Button>
-        </chakra.form>
-      </Container>
+      <Flex alignItems={'start'}>
+        <Avatar />
+        <Box ml={2}>
+          <Text bgColor={'gray.200'} rounded={'md'} px={2} py={1}>
+            {message}
+          </Text>
+        </Box>
+      </Flex>
     )
+  }
+  
+  export const Page = () => {
+   const [message, setMessage] = useState<string>('')
+  
+   const handleSendMessage = async (e: FormEvent<HTMLFormElement>) => {
+     e.preventDefault()
+     try {
+       const db = getDatabase()
+       const dbRef = ref(db, 'chat')
+       await push(dbRef, {
+         message,
+       })
+       setMessage('')
+     } catch (e) {
+       if (e instanceof FirebaseError) {
+         console.log(e)
+       }
+     }
    }
-   
-   export default Page
+
+   const [chats, setChats] = useState<{ message: string }[]>([])
+
+   useEffect(() => {
+     try {
+       const db = getDatabase()
+       const dbRef = ref(db, 'chat')
+       return onChildAdded(dbRef, (snapshot) => {
+         const message = String(snapshot.val()['message'] ?? '')
+         setChats((prev) => [...prev, { message }])
+       })
+     } catch (e) {
+       if (e instanceof FirebaseError) {
+         console.error(e)
+       }
+       return
+     }
+     // eslint-disable-next-line react-hooks/exhaustive-deps
+   }, [])
+  
+   return (
+     <Container py={14}>
+       <Heading>チャット</Heading>
+        <Spacer height={4} aria-hidden />
+        <Flex flexDirection={'column'} overflowY={'auto'} gap={2} height={400}>
+          {chats.map((chat, index) => (
+           <Message message={chat.message} key={`ChatMessage_${index}`} />
+          ))}
+        </Flex>
+        <Spacer height={2} aria-hidden />
+       <chakra.form display={'flex'} gap={2} onSubmit={handleSendMessage}>
+         <Input value={message} onChange={(e) => setMessage(e.target.value)} />
+         <Button type={'submit'}>送信</Button>
+       </chakra.form>
+     </Container>
+   )
+  }
+  
+  export default Page
